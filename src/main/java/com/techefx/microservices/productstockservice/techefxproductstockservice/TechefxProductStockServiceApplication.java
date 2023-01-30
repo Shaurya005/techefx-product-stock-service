faefd4bb3c2efcf524eb3bef1a889694c9a223e4
Shaurya Jaiswal <shaurya.jaiswal@clover.com>
2023-01-30 13:13:14 +0530

    Commenting the explanation

--- src/main/java/com/techefx/microservices/productstockservice/techefxproductstockservice/TechefxProductStockServiceApplication.java
+++ src/main/java/com/techefx/microservices/productstockservice/techefxproductstockservice/TechefxProductStockServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// In earlier versions, we also had to mention DiscoveryClient annotation here for registering with naming server but now it's no longer mandatory.
 public class TechefxProductStockServiceApplication {
 
     public static void main(String[] args) {
